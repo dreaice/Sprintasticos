86bd8f8eed06a946354caa08a45210c905fe4fa8
Andrea Eunice <ae.aguirre.contacto@gmail.com>
2025-06-05 14:38:08 -0600

    feature: se borraron carpetas duplicadas y se cambio el nombre de la carpeta administrador por admin

--- home/home.js
+++
@@ -1,9 +0,0 @@
-// Estructura base: 
-
-// home.js
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Página Home cargada");
-    // Aquí puedes agregar eventos o funciones JS personalizados
-  });
-  
