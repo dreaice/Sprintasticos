06576691dd0baebfd2588a9d5c91caf8ec80ef0f
JoshuaGinez <jgineze1600@alumno.ipn.mx>
2025-06-05 14:18:18 -0600

    feature: se modifica la estructura de directorios

---
+++ src/pages/home/home.js
@@ -0,0 +1,9 @@
+// Estructura base: 
+
+// home.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página Home cargada");
+    // Aquí puedes agregar eventos o funciones JS personalizados
+  });
+  
