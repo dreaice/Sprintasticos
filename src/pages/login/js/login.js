d33cc9a9a135b897fe3f2314bbf296705fe623a0
Maria Gonzalez <airamjgr@gmail.com>
2025-06-16 21:22:47 -0600

    feature: implementación de validaciones de formulario de inicio de sesión

--- src/pages/login/js/login.js
+++ src/pages/login/js/login.js
@@ -1,11 +1,65 @@
 import { activarTogglePassword } from './togglePassword.js';
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
   activarTogglePassword();
 });
 
+// Validacipón de usuarios
+const usuario = {
+  usuario1: {
+    nom: "AMATISTA",
+    password:"12345"
+  },
+  usuario2: {
+    nom: "majo",
+    password:"ABCDE"
+  }
+
+};
 
 
+document.getElementById("formularioLogin").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const usuario = document.getElementById("usuario").value.trim();
+  const contrasena = document.getElementById("contrasena").value.trim();
+
+
+    // Validación de campos vacíos
+    
+  if (!usuario || !contrasena) {
+    mostrarAlerta('Todos los campos son obligatorios', 'danger');
+    return;
+  } 
+  validacion(usuario, contrasena);
+  
+});
+
+function validacion(userIngresado, contrasena){
+  const user = usuario[userIngresado];
+  if(user && user.password === contrasena){
+    mostrarAlerta('¡Bienvenido!');
+    setTimeout(() => {
+      const nombre = localStorage.setItem("userIngresado", user);
+      window.location.href = "../../../../index.html";
+    }, 1000);   
+  } else {
+    mostrarAlerta('Usuario o contraseña incorrectos');
+  }
+}
+
+
+  function mostrarAlerta(mensaje, tipo = 'danger') {
+    const alertaHTML = `
+      <div class="alert alert-${tipo} alert-dismissible fade show mt-3" role="alert">
+        ${mensaje}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Cerrar"></button>
+      </div>
+    `;
+    document.getElementById('alertContainer').innerHTML = alertaHTML;
+  }
 
 
 // document.getElementById('formularioRegistro').addEventListener('submit', function (event) {
