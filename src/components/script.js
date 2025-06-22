5e5cb6198ef134be1fb4aa70c8f7578093ede3e9
Eduardo Landeros <ls.eduardo.11@gmail.com>
2025-06-22 11:25:28 -0600

    se agregaron los modulos de informacion al cotizador secundario

--- src/components/script.js
+++ src/components/script.js
@@ -1,11 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { // Esta línea espera a que el DOM esté completamente cargado (todo el HTML esté disponible en el navegador), antes de ejecutar el código dentro.
   // Cargar navbar
-  fetch("/src/components/navbar.html")
-    .then(res => res.text())
+  fetch("/src/components/navbar.html") //Realiza una petición HTTP GET al archivo local
+    .then(res => res.text()) // Convierte la respuesta (que es un objeto Response) en texto plano (el HTML).
     .then(html => {
       document.getElementById("navbar-container").innerHTML = html;
-    });
+    }); // Inserta ese HTML como contenido dentro del <div id="navbar-container">...</div> de la página actual
 
+
+    
   // Cargar footer
   fetch("/src/components/footer.html")
     .then(res => res.text())
